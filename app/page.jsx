24bd5339b55c684ed7da3efd8714a8e53c104a1a
atikur55 <atikcseuu40@gmail.com>
2024-10-14 14:43:42 +0600

    "first commit"

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,15 @@
+import rooms from "@/data/rooms.json";
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
 export default function Home() {
   return (
     <>
-      <h1>Bookit App</h1>
+      <Heading title="Available Room" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No Rooms Found</p>
+      )}
     </>
   );
 }
